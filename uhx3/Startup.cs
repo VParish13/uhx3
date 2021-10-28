bba4a3d72e7d3ae9d08c060040a82b735f98a3e5
S. Vincent Parish <vparish13@gmail.com>
2021-10-27 19:49:06 -0500

    Added swashbuckler swagger to solution for Http API testing

--- uhx3/Startup.cs
+++ uhx3/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace uhx3
 {
@@ -26,6 +27,12 @@ namespace uhx3
 			 {
 				 configuration.RootPath = "ClientApp/dist";
 			 } );
+
+			// Swagger generates an API test page to confirm your HTTP requests are functioning. 
+			services.AddSwaggerGen( c =>
+			 {
+				 c.SwaggerDoc( "v0", new OpenApiInfo { Title = "UHX API", Version = "v0" } );
+			 } );
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +48,23 @@ namespace uhx3
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
+
+			// Register and define swagger
+			/* 
+			 * All HTTP paths must be decorated:
+			 * [Route("example"]
+			 * public class ExampleController : Controller
+			 * [HttpGet()] --or HttpPost or HttpDelete etc.
+			 * public IActionResult DoStuff(){}
+			 * 
+			 * see swagger documentation: https://github.com/domaindrivendev/Swashbuckle.AspNetCore#getting-started
+			*/
+			app.UseSwagger();
+			app.UseSwaggerUI( c =>
+			 {
+				// View swagger page at https://uhx3/swagger
+				c.SwaggerEndpoint( "v0/swagger.json", "UHX API v0" );
+			 } );
 
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
