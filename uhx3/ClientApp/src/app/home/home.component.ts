0fa26b0a493b74ba55545e698bd105b576d1192d
S. Vincent Parish <vparish13@gmail.com>
2021-10-28 20:17:37 -0500

    Figured out the issue - primary css file being referenced couldn't be found by home page. Removing the reference allows the home page to load.

--- uhx3/ClientApp/src/app/home/home.component.ts
+++ uhx3/ClientApp/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['../../styles.css', './home.component.css']
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 }
