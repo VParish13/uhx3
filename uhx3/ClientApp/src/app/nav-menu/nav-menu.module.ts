9fe1d57c8d4e80bb27da659ed584afb30a86389a
S. Vince Parish <58963758+VParish13@users.noreply.github.com>
2022-03-31 10:47:45 -0500

    Made some navmenu changes. Check it out.  (#14)
    * Navmenu replaced home page and routing works. Added ngIf to nav html so links disappear after leaving home and return when returning home. Have a space saver for the hamburger shield - css needs some work. Also, want to add a swipe effect when navigating away (css effect? or javascript?).
    
    * Updated style css to have variable colors and fonts - we only have to change it in one place for future updates. Nav menu correctly fades in and out with a tangible delay of .1s. Hamburger placeholder is on left where we want it. Need to add a collapse to the hamburger action instead of an immediate change so it appears more organic.

---
+++ uhx3/ClientApp/src/app/nav-menu/nav-menu.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavMenuComponent } from './nav-menu.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    NavMenuComponent
+  ],
+  providers: []
+})
+export class NavMenuModule { }
