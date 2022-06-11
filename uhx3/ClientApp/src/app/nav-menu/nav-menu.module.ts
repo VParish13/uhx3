589391b01a172ff3e2eedf65f16771810f798cf2
S. Vincent Parish <vparish13@gmail.com>
2022-06-10 23:50:24 -0500

    Pushing to d/l on the new PC. Should be nav menu changes and unsuccessful shield graphic

--- uhx3/ClientApp/src/app/nav-menu/nav-menu.module.ts
+++ uhx3/ClientApp/src/app/nav-menu/nav-menu.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { NavMenuComponent } from './nav-menu.component';
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
     CommonModule
   ],
   declarations: [
