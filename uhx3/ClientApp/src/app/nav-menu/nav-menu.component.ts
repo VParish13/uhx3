85b697cae68a572ef5dc32fa47ab114772fc869e
Vinny Parish <vparish13@gmail.com>
2022-06-18 09:30:11 -0500

    Made the navmenu and home shield animated. Added a smaller shield image so it would load faster.

--- uhx3/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ uhx3/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,27 +1,33 @@
 import { Component } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.css'],
+  animations: [
+    trigger('slideOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('100ms', style({ transform: 'translateX(0%)' }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 
 export class NavMenuComponent {
   atHome = true;
 
-  //collapse() {
-  //  this.isExpanded = false;
-  //}
-
-  //toggle() {
-  //  this.isExpanded = !this.isExpanded;
-  //}
   LeaveHome() {
-    this.atHome = false;
+    setTimeout(() => {
+      this.atHome = false;
+    }, 200)
   }
 
   ReturnHome() {
-    console.log('clicked button');
     this.atHome = true;
   }
 }
