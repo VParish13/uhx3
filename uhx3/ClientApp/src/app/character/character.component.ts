de0a07e25f35c415680bcb8a34c4d05bfe2a75f1
Vinny Parish <vparish13@gmail.com>
2022-07-04 10:02:04 -0500

    Initial commit for character home page

--- uhx3/ClientApp/src/app/character/character.component.ts
+++ uhx3/ClientApp/src/app/character/character.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
 })
 
 export class CharacterComponent {
+  leftTile: SelectTile[] = [
+    { text: 'New Character', cols: 1, rows: 1, gutterSize: "10px" }
+  ];
+  rightTiles: SelectTile[] = [
+    { text: 'Char1', cols: 1, rows: 1, gutterSize: "10px" },
+    { text: 'Char2', cols: 1, rows: 1, gutterSize: "10px" },
+    { text: 'Char3', cols: 1, rows: 1, gutterSize: "10px" },
+    { text: 'Char4', cols: 1, rows: 1, gutterSize: "10px" }
+  ];
+}
 
+
+// Move this somewhere general and call in for each component
+// Must have forms capability to display variable text (ex. character name, encounter name, etc.)
+export interface SelectTile {
+  cols: number;
+  rows: number;
+  text: string;
+  gutterSize: string;
 }
