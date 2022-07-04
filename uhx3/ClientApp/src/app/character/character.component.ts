3c67e78c4fc15225da0951ccff7c1a7ca35b9c30
S. Vince Parish <58963758+VParish13@users.noreply.github.com>
2022-07-04 10:00:09 -0500

    Dev/angular material install (#16)
    * commit for character changes. will also commit angular improvements.
    
    * node and typescript changes
    
    * fixed dependencies and confirmed angular components working
    
    * Angular materials working! Updated css files to scss to create a customized angular material theme with our palette.
    
    * Custom color theme for Angular material components added
    
    * Added the basic mat grid list to character screen as a final test for angular material install

---
+++ uhx3/ClientApp/src/app/character/character.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'character',
+  templateUrl: './character.component.html',
+  styleUrls: ['./character.component.scss']
+})
+
+export class CharacterComponent {
+
+}
--- uhx3/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ uhx3/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -4,15 +4,15 @@ import { trigger, transition, animate, style } from '@angular/animations';
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css'],
+  styleUrls: ['./nav-menu.component.scss'],
   animations: [
     trigger('slideOut', [
       transition(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('100ms', style({ transform: 'translateX(0%)' }))
+        style({ transform: 'translateY(-70%)' }),
+        animate('100ms', style({ transform: 'translateY(0%)' }))
       ]),
       transition(':leave', [
-        animate('200ms ease-in', style({ transform: 'translateX(-100%)' }))
+        animate('100ms', style({ transform: 'translateX(100%)' }))
       ])
     ])
   ]
@@ -22,9 +22,7 @@ export class NavMenuComponent {
   atHome = true;
 
   LeaveHome() {
-    setTimeout(() => {
-      this.atHome = false;
-    }, 200)
+    this.atHome = false;
   }
 
   ReturnHome() {
